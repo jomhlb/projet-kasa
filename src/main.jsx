ee7fc3c3330b64f8cc41a64ed0312d87db2117af
jomhlb <johannamuhlbergerr@gmail.com>
2025-08-20 16:29:01 +0200

    maj fichier main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './router';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import Layout from './components/Layout';
+import ApartmentDetails from './pages/AppartmentDetails';
+
 import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route path="/apartments/:id" element={<ApartmentDetails />} />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <About />
+            </Layout>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <Error />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
